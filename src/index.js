bbde40ac1333205be912ab070fe28ff5bf540911
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-01-27 00:31:44 -0500

    modificar url backend

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import store from './redux/store/store.js'
 import axios from 'axios'
 
 // axios.defaults.baseURL = "http://localhost:3001"
-axios.defaults.baseURL = "pi-food-back-production-ddb7.up.railway.app"
+axios.defaults.baseURL = "https://pi-food-henry.up.railway.app"
 
 ReactDOM.render(
   <Provider store={store}>
